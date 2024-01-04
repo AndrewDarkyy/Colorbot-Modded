443e0d56fa4d70d4ec290262da3849b3981fe875
AndrewDarkyy <129768810+AndrewDarkyy@users.noreply.github.com>
2024-01-04 21:53:03 +0300

    Update aimsource.py

--- aimsource.py
+++ aimsource.py
@@ -13,66 +13,58 @@ from ctypes import windll
 from time import sleep
 from threading import Thread
 from urllib.request import urlopen
+from webbrowser import open
 from pygetwindow import getActiveWindow
 
-range=range
-len=len
-print=print
-tuple=tuple
-int=int
-max=max
+range = range
+len = len
+print = print
+tuple = tuple
+int = int
+max = max
+sleep = sleep
 
 user32 = windll.user32
 
-CURRENT_VERSION = "v1.5" # IMPORTANT !!!!!! CHANGE lmfao
+switchmodes = ("Hold", "Toggle")
+resetall = Style.RESET_ALL
 
 system("title Colorbot")
 
-if urlopen("https://raw.githubusercontent.com/AndrewDarkyy/Colorbot-Modded/main/version.txt").read().decode("utf-8")!=CURRENT_VERSION+"\n":
-    print(Style.BRIGHT + Fore.CYAN + "This version is outdated, please get the latest one at " + Fore.YELLOW + "https://github.com/AndrewDarkyy/Colorbot-Modded/releases" + Style.RESET_ALL)
+if urlopen("https://raw.githubusercontent.com/AndrewDarkyy/Colorbot-Modded/main/version.txt").read().decode("utf-8") != "v1.6\n":
+    print(Style.BRIGHT + Fore.CYAN + "This version is outdated, please get the latest one at " + Fore.YELLOW + "https://github.com/AndrewDarkyy/Colorbot-Modded/releases" + resetall)
+    open("https://github.com/AndrewDarkyy/Colorbot-Modded/releases")
     while True:
         pass
 
-switchmodes = ("Hold", "Toggle")
-
-sdir = path.dirname(path.abspath(__file__))
-
-config = ConfigParser()
-config.optionxform = str
-config.read(path.join(sdir, "config.ini"))
-
-def check_key(string, key):
-    if string == "LeftClick" or string == "win32con.VK_XBUTTON1" or string == "VK_XBUTTON1":
+def string_tokey(string, key):
+    if string.lower() == "leftclick":
         return 0x01
-    elif string == "RightClick" or string == "win32con.VK_XBUTTON2" or string == "VK_XBUTTON2":
+    elif string.lower() == "rightclick":
         return 0x02
-    elif string == "MiddleClick" or string == "win32con.VK_XBUTTON3" or string == "VK_XBUTTON3":
+    elif string.lower() == "middleclick":
         return 0x04
-    elif string == "SideButton1" or string == "win32con.VK_XBUTTON4" or string == "VK_XBUTTON4":
+    elif string.lower() == "sidebutton1":
         return 0x05
-    elif string == "SideButton2" or string == "win32con.VK_XBUTTON5" or string == "VK_XBUTTON5":
+    elif string.lower() == "sidebutton2":
         return 0x06
-    elif string == "Disabled":
-        return "Disabled"
     else:
         try:
             is_pressed(string)
         except:
             print(f"Please change {key} to an existing key.")
             while True:
-                sleep(0.1) # omggg check if key exists so just, just amazing
-                try:
-                    new_config = ConfigParser()
-                    new_config.optionxform = str
-                    new_config.read(path.join(sdir, "config.ini"))
-                    is_pressed(new_config.get("Config", key))
-                    string = new_config.get("Config", key)
-                    break
-                except:
-                    pass
+                pass
         return string
     
-def print_banner_stuffz(key):
+
+def is_roblox_focused():
+    try:
+        return "Roblox" in getActiveWindow().title
+    except:
+        return False
+    
+def key_tostring(key):
     if key == 0x01:
         return "LeftClick"
     elif key == 0x02:
@@ -83,62 +75,61 @@ def print_banner_stuffz(key):
         return "SideButton1"
     elif key == 0x06:
         return "SideButton2"
-    elif key == "Disabled":
-        return "Disabled"
     else:
         return str(key)
-
-def is_roblox_focused():
-    try:
-        return getActiveWindow().title == "Roblox"
-    except:
-        pass
-
-try:
-    global A1M_KEY, SWITCH_MODE_KEY
-    global CAM_FOV, A1M_FOV, TRIGGERBOT_DELAY
-    global A1M_OFFSET_Y, A1M_OFFSET_X, A1M_SPEED_X, A1M_SPEED_Y
-    global upper, lower
-    A1M_KEY = check_key(config.get("Config", "A1M_KEY"), "A1M_KEY")
-    SWITCH_MODE_KEY = check_key(config.get("Config", "SWITCH_MODE_KEY"), "SWITCH_MODE_KEY")
-    CAM_FOV = int(config.get("Config", "CAM_FOV"))
-    A1M_FOV = int(config.get("Config", "A1M_FOV"))
-    TRIGGERBOT_DELAY = float(config.get("Config", "TRIGGERBOT_DELAY"))
-    A1M_OFFSET_Y = int(config.get("Config", "A1M_OFFSET_Y"))
-    A1M_OFFSET_X = int(config.get("Config", "A1M_OFFSET_X"))
-    A1M_SPEED_X = float(config.get("Config", "A1M_SPEED_X"))
-    A1M_SPEED_Y = float(config.get("Config", "A1M_SPEED_Y"))
-    upper = array((38, 255, 203), dtype="uint8")
-    lower = array((30, 255, 201), dtype="uint8")
     
-    if A1M_SPEED_X <= 0:
-        A1M_SPEED_X = 0.001 # everything multiplied by 0 is 0 (except infinity) so no use
-    if A1M_SPEED_X > 1:
-        A1M_SPEED_X = 1 # more than one prob useless
+try:
+    config = ConfigParser()
+    config.optionxform = str
+    config.read(path.join(path.dirname(path.dirname(__file__)), "config.ini"))
+    global AIM_KEY, SWITCH_MODE_KEY, AIM_FOV, TRIGGERBOT_DELAY, AIM_OFFSET_Y, AIM_OFFSET_X, AIM_SPEED_X, AIM_SPEED_Y, COLOR, lower, upper, colorname
+    
+    AIM_KEY = string_tokey(config.get("Config", "AIM_KEY"), "AIM_KEY")
+    SWITCH_MODE_KEY = string_tokey(config.get("Config", "SWITCH_MODE_KEY"), "SWITCH_MODE_KEY")
+    AIM_FOV = int(config.get("Config", "AIM_FOV"))
+    TRIGGERBOT_DELAY = float(config.get("Config", "TRIGGERBOT_DELAY"))
+    AIM_SPEED_X = float(config.get("Config", "AIM_SPEED_X"))
+    AIM_SPEED_Y = float(config.get("Config", "AIM_SPEED_Y"))
+    AIM_OFFSET_Y = int(config.get("Config", "AIM_OFFSET_Y"))
+    AIM_OFFSET_X = int(config.get("Config", "AIM_OFFSET_X"))
+    COLOR = str(config.get("Config", "COLOR"))
 
-    if 0 > TRIGGERBOT_DELAY:
-        TRIGGERBOT_DELAY = 0
-
-    if A1M_SPEED_Y <= 0:
-        A1M_SPEED_Y = 0.001
-    if A1M_SPEED_Y > 1:
-        A1M_SPEED_Y = 1
+    if COLOR.lower() == "blue":
+        colorname = Fore.BLUE
+        upper = array((123, 255, 217), dtype="uint8")
+        lower = array((113, 206, 189), dtype="uint8")
+    elif COLOR.lower() == "pink" or COLOR.lower() == "purple":
+        colorname = Fore.MAGENTA
+        upper = array((150, 255, 201), dtype="uint8")
+        lower = array((150, 255, 200), dtype="uint8")
+    elif COLOR.lower() == "green":
+        colorname = Fore.GREEN
+        upper = array((60, 255, 201), dtype="uint8")
+        lower = array((60, 255, 201), dtype="uint8")
+    elif COLOR.lower() == "cyan":
+        colorname = Fore.CYAN
+        upper = array((90, 255, 201), dtype="uint8")
+        lower = array((90, 255, 201), dtype="uint8")
+    else:
+        colorname = Fore.YELLOW
+        upper = array((38, 255, 203), dtype="uint8")
+        lower = array((30, 255, 201), dtype="uint8")
 except Exception as e:
-    print("Error loading settings:", e)
+    print("Error occurred while loading settings:", e)
 
 sct = mss()
 
-center = CAM_FOV / 2
+center = AIM_FOV / 2
 
 screenshot = sct.monitors[1]
 screenshot["left"] = int((screenshot["width"] / 2) - center)
 screenshot["top"] = int((screenshot["height"] / 2) - center)
-screenshot["width"] = CAM_FOV
-screenshot["height"] = CAM_FOV
+screenshot["width"] = AIM_FOV
+screenshot["height"] = AIM_FOV
 
 ones_uint = ones((3, 3), uint8)
-sct_grab=sct.grab
-m_event=user32.mouse_event
+sct_grab = sct.grab
+m_event = user32.mouse_event
 
 class colorbot:
     def __init__(self):
@@ -154,7 +145,7 @@ class colorbot:
             m_event(0x0004)
 
     def __delayedaim(self):
-        self.__shooting=True
+        self.__shooting = True
         sleep(TRIGGERBOT_DELAY)
         m_event(0x0002)
         self.__clicks += 1
@@ -167,13 +158,13 @@ class colorbot:
             if len(contours) != 0:
                 contour = max(contours, key=contourArea)
                 topmost = tuple(contour[contour[:, :, 1].argmin()][0])
-                x = topmost[0] - center + A1M_OFFSET_X
-                y = topmost[1] - center + A1M_OFFSET_Y
+                x = topmost[0] - center + AIM_OFFSET_X
+                y = topmost[1] - center + AIM_OFFSET_Y
                 distance = sqrt(x*x + y*y)
-                if distance <= A1M_FOV:
-                    m_event(0x0001, int(x * A1M_SPEED_X), int(y * A1M_SPEED_Y), 0, 0)
-                if distance<=8:
-                    if TRIGGERBOT_DELAY!=0:
+                if distance <= AIM_FOV:
+                    m_event(0x0001, int(x * AIM_SPEED_X), int(y * AIM_SPEED_Y), 0, 0)
+                if distance <= 9:
+                    if TRIGGERBOT_DELAY != 0:
                         if self.__shooting == False:
                             Thread(target = self.__delayedaim).start()
                     else:
@@ -183,11 +174,11 @@ class colorbot:
                 elif distance<=50:
                     for index in range(len(contour)):
                         topmost = tuple(contour[index][0])
-                        x = topmost[0] - center + A1M_OFFSET_X
-                        y = topmost[1] - center + A1M_OFFSET_Y
+                        x = topmost[0] - center + AIM_OFFSET_X
+                        y = topmost[1] - center + AIM_OFFSET_Y
                         distance = sqrt(x*x + y*y)
-                        if distance<=7:
-                            if TRIGGERBOT_DELAY!=0:
+                        if distance <= 8:
+                            if TRIGGERBOT_DELAY != 0:
                                 if self.__shooting == False:
                                     Thread(target = self.__delayedaim).start()
                             else:
@@ -196,7 +187,7 @@ class colorbot:
                                 Thread(target = self.__stop).start()
                             break
 
-    def a1mtoggle(self):
+    def aimtoggle(self):
         self.aimtoggled = not self.aimtoggled
         sleep(.08)
 
@@ -207,106 +198,105 @@ class colorbot:
             self.switchmode = 0
         sleep(.08)
 
-def print_banner(bot: colorbot):
+def print_banner(bot):
     system("cls")
-    print(Style.BRIGHT + Fore.CYAN + "Colorbot for Arsenal!" + Style.RESET_ALL)
-    print("====== Controls ======")
-    print("Aimbot Keybind      :", Fore.YELLOW + print_banner_stuffz(A1M_KEY) + Style.RESET_ALL)
-    print("Change Mode         :", Fore.YELLOW + print_banner_stuffz(SWITCH_MODE_KEY) + Style.RESET_ALL)
-    print("==== Information =====")
-    print("Aimbot Mode         :", Fore.CYAN + switchmodes[bot.switchmode] + Style.RESET_ALL)
-    print("Aimbot FOV          :", Fore.CYAN + str(A1M_FOV) + Style.RESET_ALL)
-    print("Camera FOV          :", Fore.CYAN + str(CAM_FOV) + Style.RESET_ALL)
-    print("Shoot Delay         :", Fore.CYAN + str(TRIGGERBOT_DELAY) + Style.RESET_ALL)
-    print("Sensitivity         :", Fore.CYAN + "X: " + str(A1M_SPEED_X) + " Y: " + str(A1M_SPEED_Y) + Style.RESET_ALL)
-    print("Offset              :", Fore.CYAN + "X: " + str(A1M_OFFSET_X) + " Y: " + str(A1M_OFFSET_Y) + Style.RESET_ALL)
-    print("Aiming              :", (Fore.GREEN if bot.aimtoggled else Fore.RED) + str(bot.aimtoggled))
+    print(Style.BRIGHT + Fore.CYAN + "Colorbot for Arsenal!" + Fore.RED + " Credits to @andrewdarkyy. and @seconb" + resetall)
+    print(Style.BRIGHT + Fore.GREEN + "Join our discord server for updates https://discord.gg/hH62fKGJnv" + resetall)
+    print(Style.BRIGHT + Fore.MAGENTA + "Make sure you fullscreen your Roblox window and are in the web version!" + resetall)
+    print(Style.BRIGHT + Fore.YELLOW + "====== Controls ======" + resetall)
+    print("Aimbot Keybind    :", Fore.YELLOW + key_tostring(AIM_KEY) + resetall)
+    print("Change Mode       :", Fore.YELLOW + key_tostring(SWITCH_MODE_KEY) + resetall)
+    print(Style.BRIGHT + Fore.YELLOW + "==== Information =====" + resetall)
+    print("Aimbot Mode       :", Fore.CYAN + switchmodes[bot.switchmode] + resetall)
+    print("Aimbot FOV        :", Fore.CYAN + str(AIM_FOV) + resetall)
+    print("Shoot Delay       :", Fore.CYAN + str(TRIGGERBOT_DELAY) + resetall)
+    print("Sensitivity       :", Fore.CYAN + "X: " + str(AIM_SPEED_X) + " Y: " + str(AIM_SPEED_Y) + resetall)
+    print("Offset            :", Fore.CYAN + "X: " + str(AIM_OFFSET_X) + " Y: " + str(AIM_OFFSET_Y) + resetall)
+    print("Aiming            :", (Fore.GREEN if bot.aimtoggled else Fore.RED) + str(bot.aimtoggled) + resetall)
+    print("Enemy Color       :", Fore.CYAN + str(Style.NORMAL + colorname + COLOR))
 
-del CURRENT_VERSION
-del config
-del sdir
-del check_key
-del urlopen
-del mss
-del ConfigParser
+open("https://discord.gg/hH62fKGJnv")
 
 if __name__ == "__main__":
     bot = colorbot()
     del colorbot
+    del urlopen
+    del mss
+    del open
+    del ConfigParser
+    del string_tokey
     print_banner(bot)
     while True:
-        if SWITCH_MODE_KEY != "Disabled":
-            if SWITCH_MODE_KEY == 0x01 or SWITCH_MODE_KEY == 0x02 or SWITCH_MODE_KEY == 0x04:
-                if GetAsyncKeyState(SWITCH_MODE_KEY) < 0:
-                    bot.modeswitch()
-                    print_banner(bot)
-            elif SWITCH_MODE_KEY == 0x05 or SWITCH_MODE_KEY == 0x06:
-                if bool(user32.GetKeyState(SWITCH_MODE_KEY) & 0x80):
-                    bot.modeswitch()
-                    print_banner(bot)
-            elif is_pressed(SWITCH_MODE_KEY):
+        if SWITCH_MODE_KEY == 0x01 or SWITCH_MODE_KEY == 0x02 or SWITCH_MODE_KEY == 0x04:
+            if GetAsyncKeyState(SWITCH_MODE_KEY) < 0:
                 bot.modeswitch()
                 print_banner(bot)
-
+        elif SWITCH_MODE_KEY == 0x05 or SWITCH_MODE_KEY == 0x06:
+            if bool(user32.GetKeyState(SWITCH_MODE_KEY) & 0x80):
+                bot.modeswitch()
+                print_banner(bot)
+        elif is_pressed(SWITCH_MODE_KEY):
+            bot.modeswitch()
+            print_banner(bot)
+        
         sleep(0.1)
 
-        if A1M_KEY != "Disabled":
-            if A1M_KEY == 0x02 or A1M_KEY == 0x01 or A1M_KEY == 0x04:
-                if GetAsyncKeyState(A1M_KEY) < 0:
-                    if bot.switchmode == 0:
-                        while GetAsyncKeyState(A1M_KEY) < 0:
-                            if not bot.aimtoggled:
-                                bot.a1mtoggle()
-                                print_banner(bot)
-                                while bot.aimtoggled: 
-                                    bot.process()
-                                    if not GetAsyncKeyState(A1M_KEY) < 0:
-                                        bot.a1mtoggle()
-                                        print_banner(bot)
-                    else:
-                        bot.a1mtoggle()
-                        print_banner(bot)
-                        while bot.aimtoggled:
-                            bot.process()
-                            if GetAsyncKeyState(A1M_KEY) < 0:
-                                bot.a1mtoggle()
-                                print_banner(bot)
-            elif A1M_KEY == 0x05 or A1M_KEY == 0x06:
-                if bool(user32.GetKeyState(A1M_KEY) & 0x80):
-                    if bot.switchmode == 0:
-                        while bool(user32.GetKeyState(A1M_KEY) & 0x80):
-                            if not bot.aimtoggled:
-                                bot.a1mtoggle()
-                                print_banner(bot)
-                                while bot.aimtoggled: 
-                                    bot.process()
-                                    if not bool(user32.GetKeyState(A1M_KEY) & 0x80):
-                                        bot.a1mtoggle()
-                                        print_banner(bot)
-                    else:
-                        bot.a1mtoggle()
-                        print_banner(bot)
-                        while bot.aimtoggled:
-                            bot.process()
-                            if bool(user32.GetKeyState(A1M_KEY) & 0x80):
-                                bot.a1mtoggle()
-                                print_banner(bot)
-            elif is_pressed(A1M_KEY):
+        if AIM_KEY == 0x02 or AIM_KEY == 0x01 or AIM_KEY == 0x04:
+            if GetAsyncKeyState(AIM_KEY) < 0:
                 if bot.switchmode == 0:
-                    while is_pressed(A1M_KEY):
+                    while GetAsyncKeyState(AIM_KEY) < 0:
                         if not bot.aimtoggled:
-                            bot.a1mtoggle()
+                            bot.aimtoggle()
                             print_banner(bot)
-                            while bot.aimtoggled:
+                            while bot.aimtoggled: 
                                 bot.process()
-                                if not is_pressed(A1M_KEY):
-                                    bot.a1mtoggle()
+                                if not GetAsyncKeyState(AIM_KEY) < 0:
+                                    bot.aimtoggle()
                                     print_banner(bot)
-                else: 
-                    bot.a1mtoggle()
+                else:
+                    bot.aimtoggle()
                     print_banner(bot)
                     while bot.aimtoggled:
                         bot.process()
-                        if is_pressed(A1M_KEY):
-                            bot.a1mtoggle()
+                        if GetAsyncKeyState(AIM_KEY) < 0:
+                            bot.aimtoggle()
                             print_banner(bot)
+        elif AIM_KEY == 0x05 or AIM_KEY == 0x06:
+            if bool(user32.GetKeyState(AIM_KEY) & 0x80):
+                if bot.switchmode == 0:
+                    while bool(user32.GetKeyState(AIM_KEY) & 0x80):
+                        if not bot.aimtoggled:
+                            bot.aimtoggle()
+                            print_banner(bot)
+                            while bot.aimtoggled: 
+                                bot.process()
+                                if not bool(user32.GetKeyState(AIM_KEY) & 0x80):
+                                    bot.aimtoggle()
+                                    print_banner(bot)
+                else:
+                    bot.aimtoggle()
+                    print_banner(bot)
+                    while bot.aimtoggled:
+                        bot.process()
+                        if bool(user32.GetKeyState(AIM_KEY) & 0x80):
+                            bot.aimtoggle()
+                            print_banner(bot)
+        elif is_pressed(AIM_KEY):
+            if bot.switchmode == 0:
+                while is_pressed(AIM_KEY):
+                    if not bot.aimtoggled:
+                        bot.aimtoggle()
+                        print_banner(bot)
+                        while bot.aimtoggled:
+                            bot.process()
+                            if not is_pressed(AIM_KEY):
+                                bot.aimtoggle()
+                                print_banner(bot)
+            else: 
+                bot.aimtoggle()
+                print_banner(bot)
+                while bot.aimtoggled:
+                    bot.process()
+                    if is_pressed(AIM_KEY):
+                        bot.aimtoggle()
+                        print_banner(bot)
